2c3b9dc34bf7e27881e2149bbf6633f0e1cb16bd
Chandra179 <chandrafirst67@gmail.com>
2022-04-22 21:16:52 +0700

    'add'

--- db.js
+++ db.js
@@ -2,19 +2,26 @@ const MongoClient = require("mongodb").MongoClient;
 
 // Mognodb atlas url
 const uri =
-  "mongodb+srv://chandra:admin@cluster0.mvccd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const options = { useUnifiedTopology: true, useNewUrlParser: true }
+  "mongodb+srv://admin:admin@cluster0.myiqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const options = { useUnifiedTopology: true, useNewUrlParser: true };
 var db;
 
+/**
+ * callback is a function called at the completion of a given task;
+ * this prevents any blocking, and allows other code to be run in the meantime.
+ * The callback gets called after the function is done with all of its operations
+ */
 function connectToServer(callback) {
-  MongoClient.connect(
-    uri,
-    options,
-    function (err, client) {
-      db = client.db("sample_airbnb");
-      return callback(err);
-    }
-  );
+  MongoClient.connect(uri, options, function (err, client) { //error callback conventions
+    db = client.db("sample_airbnb");
+    /**
+     * If the function hits an error, then they typically call the
+     * callback with the first parameter being an Error object.
+     * If it cleanly exits, then they will call the callback with the
+     * first parameter being null and the rest being the return value(s).
+     */
+    return callback(err);
+  });
 }
 
 function getDb(collectionToGet) {
